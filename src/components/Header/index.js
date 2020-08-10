ecd68edf156f456b26ab12ce63d29f3dcba69db3
Chris Kramer <ChrisKramer15@outlook.com>
2020-08-09 23:01:37 -0500

    app is functional, correcting minor issues

---
+++ src/components/Header/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./style.css";
+
+function Header() {
+  return (
+    <div>
+      <div className="jumbotron jumbotron-fluid header">
+        <div className="container">
+          <h1>Employee Directory</h1>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
